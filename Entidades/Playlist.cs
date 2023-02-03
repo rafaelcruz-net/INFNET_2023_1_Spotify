11fbcf1d2d3ad56494f2555275e8785a46b92013
rafael.cruz <rafael.cruz@SJ102-99>
2023-02-03 09:28:11 -0300

    Add project files.

---
+++ Entidades/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Playlist
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool IsPublica { get; set;}
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }   
+        public string Nome { get; set;}
+        public String Email { get; set; }
+        public DateTime DtNascimento { get; set; }
+        public string Password { get; set; }
+        public List<Playlist> Playlists { get; set; }
+
+    }
+}
