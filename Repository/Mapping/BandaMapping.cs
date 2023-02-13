9145647139647ae32231d47e604bd602f98982a2
rafael.cruz <rafaelcruz.net81@gmail.com>
2023-02-13 07:40:03 -0300

    Fazendo mapeamento da classes

--- Repository/Mapping/BandaMapping.cs
+++ Repository/Mapping/BandaMapping.cs
@@ -9,15 +9,16 @@ using System.Threading.Tasks;
 
 namespace Repository.Mapping
 {
-    public class PlaylistMapping : IEntityTypeConfiguration<Playlist>
+    public class BandaMapping : IEntityTypeConfiguration<Banda>
     {
-        public void Configure(EntityTypeBuilder<Playlist> builder)
+        public void Configure(EntityTypeBuilder<Banda> builder)
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd();
 
-            builder.Property(x => x.IsPublica).IsRequired();
-            builder.Property(x => x.Nome).IsRequired().HasMaxLength(150);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Descricao).HasMaxLength(500);
+            builder.Property(x => x.FotoBanda).HasMaxLength(500);
         }
     }
 }
--- Repository/Mapping/UsuarioMapping.cs
+++ Repository/Mapping/UsuarioMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Entidades;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,20 @@ namespace Repository.Mapping
 {
     public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
     {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Nome)
+                   .IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Email)
+                    .IsRequired().HasMaxLength(200);
+            builder.Property(x => x.DtNascimento)
+                    .IsRequired();
+            builder.Property(x => x.Password)
+                    .IsRequired().HasMaxLength(200);
 
+            builder.HasMany(x => x.Playlists).WithOne(x => x.Usuario);
+        }
     }
 }
