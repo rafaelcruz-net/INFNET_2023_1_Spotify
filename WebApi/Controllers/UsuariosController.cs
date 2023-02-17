fe897186bf10309e8e61f01ef69973c54de30334
Rafael Cruz <rafaelcruz.net81@gmail.com>
2023-02-17 09:30:15 -0300

    Adicionando controllers de usuario para web api

---
+++ WebApi/Controllers/UsuariosController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Validations;
+using Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private SpotifyContext context;
+
+        public UsuariosController(SpotifyContext _context)
+        {
+            this.context = _context;
+        }
+
+        // GET: api/<UsuariosController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(this.context.Usuarios);
+        }
+
+        // GET api/<UsuariosController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = this.context.Usuarios.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+            
+        }
+
+        // POST api/<UsuariosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsuariosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsuariosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
